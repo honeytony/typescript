d64c57c3cad44368fa54cc46bb836f595677ad91
Anton Kucher <antonddmedia@gmail.com>
2022-11-11 20:48:58 +0300

    add

---
+++ enums.ts
@@ -0,0 +1,10 @@
+enum StatusCode {
+    Success = 1,
+    Process,
+    Error,
+}
+
+const res = {
+    message: 'Платеж успешен',
+    statusCode: StatusCode.Success,
+};
---
+++ literal.ts
@@ -0,0 +1,5 @@
+function query(url: string, method: 'get' | 'post'): string {
+    return '';
+}
+
+query('vk.com', 'post');
